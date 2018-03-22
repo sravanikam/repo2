8ae8dc69fe88a280b596d9f0c784980b1678968c
sravani <sravani@gmail.com>
2018-03-22 12:05:31 +0530

    subtraction function added

--- cal.py
+++ cal.py
@@ -8,3 +8,9 @@ sum = float(num1) + float(num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# subtract two numbers
+sum = float(num1) - float(num2)
+
+# Display the sum
+print('The subtraction of {0} and {1} is {2}'.format(num1, num2, sum))
